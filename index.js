6770dc51165de2b0ccc27190bcfcdc1d4b9d2ed3
DonMakkk <repaladhave@example.com>
2025-12-22 11:28:03 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const sendProductsData = async () => {
+  const data = await fetch("./data.json");
+  const response = await data.json();
+  return response;
+};
+
+const showProducts = async () => {
+  const productData = await sendProductsData();
+  document.getElementById("productContainer").innerHTML = productData
+    .map((element) => {
+      const { name, image, price} = element;
+      return `
+      <div class="col-6 col-md-3">
+      <div class="card h-100">
+      <img src="${image}" alt="">
+      <div class="p-3 text-center text-">
+     <p> ${name}</p>
+      <p>${price}</p>
+      </div>
+      </div>
+      </div>`;
+    })
+    .join("");
+};
+
+showProducts();
